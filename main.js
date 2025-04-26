69610970605ac071d9a6b438d081e593878d52b4
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2025-04-26 10:42:41 +0000

    sijdnaij

--- main.js
+++ main.js
@@ -49,47 +49,47 @@ for (var i = 0; i < partNumber; i++) {
   allParts.push(new Part());
 }
 
-// c.fillStyle = 'tomato';
-// c.fillRect(0, 0, cw, ch);
+c.fillStyle = 'tomato';
+c.fillRect(0, 0, cw, ch);
 
-// c.strokeStyle = 'black';
-// c.lineWidth = 2;
-// c.strokeRect(100, 100, 100, 100);
-// c.strokeRect(200, 200, 200, 200);
-// c.strokeRect(300, 300, 300, 300);
+c.strokeStyle = 'black';
+c.lineWidth = 2;
+c.strokeRect(100, 100, 100, 100);
+c.strokeRect(200, 200, 200, 200);
+c.strokeRect(300, 300, 300, 300);
 
-// c.beginPath();
-// c.moveTo(200, 200);
-// c.lineTo(300, 300);
-// c.strokeStyle = 'black';
+c.beginPath();
+c.moveTo(200, 200);
+c.lineTo(300, 300);
+c.strokeStyle = 'black';
 
-// c.strokeStyle = 'black';
-// c.beginPath();
-// c.moveTo(100, 100);
-// c.lineTo(200, 200);
-// c.lineTo(300, 100);
-// c.lineTo(100, 100);
+c.strokeStyle = 'black';
+c.beginPath();
+c.moveTo(100, 100);
+c.lineTo(200, 200);
+c.lineTo(300, 100);
+c.lineTo(100, 100);
+c.stroke();
+c.moveTo(400, 400);
+
+c.beginPath();
+c.fillStyle = 'cadetblue';
+c.arc(350, 350, 60, 0, Math.PI*2, true);
 // c.stroke();
-// c.moveTo(400, 400);
+c.fill();
 
-// c.beginPath();
-// c.fillStyle = 'cadetblue';
-// c.arc(350, 350, 60, 0, Math.PI*2, true);
-// // c.stroke();
-// c.fill();
+c.beginPath();
+c.fillStyle = 'cadetblue';
+c.arc(350, 350, 60, 0, deg(120), true);
+c.lineTo(350, 350);
+c.fill();
 
-// c.beginPath();
-// c.fillStyle = 'cadetblue';
-// c.arc(350, 350, 60, 0, deg(120), true);
-// c.lineTo(350, 350);
-// c.fill();
+function deg(deg) {
+  return deg / (180 / Math.PI);
+}
 
-// function deg(deg) {
-//   return deg / (180 / Math.PI);
-// }
-
-// c.beginPath();
-// c.fillStyle = 'cadetblue';
-// c.arc(150, 150, 60, 0, deg(360), true);
-// // c.lineTo(150, 150);
-// c.stroke();
+c.beginPath();
+c.fillStyle = 'cadetblue';
+c.arc(150, 150, 60, 0, deg(360), true);
+// c.lineTo(150, 150);
+c.stroke();
